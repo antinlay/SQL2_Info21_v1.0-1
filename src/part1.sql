7416a0a5ca26c19fa9c0224da903e129203c2e62
janiecee <janiecee@debian-BULLSEYE-live-builder-AMD64>
2023-05-02 22:34:41 +0300

    CREATE TABLES Peers, Checks, Verter

---
+++ src/part1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Peers (
+    Nickname varchar primary key,
+    Birthday date not null
+);
+CREATE TABLE Checks (
+    id bigint primary key,
+    Peer varchar not null,
+    Task varchar not null,
+    Date date not null default date current_date
+);
+CREATE TABLE Verter (
+    id bigint primary key,
+    Check bigint not null,
+    State varchar not null,
+    Time time not null default current_time
+)
