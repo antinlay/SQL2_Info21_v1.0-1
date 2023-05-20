416996cc5c2d4303b527a2dfd2ef70315966f9e2
janiecee <janiecee@debian-BULLSEYE-live-builder-AMD64>
2023-05-21 00:02:09 +0300

    develop-janiecee FRIENDS NOT WORCK CHK_PEER1_PEER2

--- src/part1.sql
+++ src/part1.sql
@@ -14,31 +14,20 @@ CREATE TABLE checks (
     id SERIAL PRIMARY KEY,
     peer VARCHAR NOT NULL,
     task VARCHAR NOT NULL,
-    date_check DATE NOT NULL,
-    CONSTRAINT fk_checks_peer FOREIGN KEY (peer) REFERENCES peers(nickname),
-    CONSTRAINT fk_checks_task FOREIGN KEY (task) REFERENCES tasks(title)
+    date_check DATE NOT NULL
 );
 CREATE TABLE p2p (
     id SERIAL PRIMARY KEY,
-    check_num BIGINT NOT NULL,
+    check_num INT NOT NULL,
     checking_peer VARCHAR NOT NULL,
     check_state check_status,
-    time_check TIME NOT NULL,
-    CONSTRAINT fk_p2p_check_num FOREIGN KEY (check_num) REFERENCES checks(id),
-    CONSTRAINT uk_p2p_check UNIQUE (check_num, checked_peer, check_state) -- WHERE check_state IN ('Start');
+    time_check TIME NOT NULL
 );
 CREATE TABLE verter (
     id SERIAL PRIMARY KEY,
     check_num INT NOT NULL,
     check_state check_status NOT NULL,
-    time_check TIME NOT NULL,
-    CONSTRAINT fk_verter_check_num FOREIGN KEY (check_num) REFERENCES checks(id)
-    WHERE EXISTS (
-            SELECT 1
-            FROM p2p
-            WHERE p2p.check_num = checks.id
-                AND p2p.check_state = 'Success'
-        );
+    time_check TIME NOT NULL
 );
 CREATE TABLE transfered_points (
     id SERIAL PRIMARY KEY,
@@ -49,33 +38,50 @@ CREATE TABLE transfered_points (
 CREATE TABLE friend (
     id SERIAL PRIMARY KEY,
     peer1 VARCHAR NOT NULL,
-    peer2 VARCHAR NOT NULL,
-    CONSTRAINT fk_friend_peer1 FOREIGN KEY (peer1) REFERENCES peers(nickname),
-    CONSTRAINT fk_friend_peer2 FOREIGN KEY (peer2) REFERENCES peers(nickname),
-    CONSTRAINT uk_peer1_peer2 UNIQUE (peer1, peer2),
-    CONSTRAINT chk_peer1_peer2 CHECK (peer1 < peer2)
+    peer2 VARCHAR NOT NULL
 );
 CREATE TABLE recomendations (
     id SERIAL PRIMARY KEY,
     peer VARCHAR NOT NULL,
-    recomended_peer VARCHAR NOT NULL,
-    CONSTRAINT fk_recomendations_peer FOREIGN KEY (peer) REFERENCES peers(nickname),
-    CONSTRAINT fk_recomendations_recomended_peer FOREIGN KEY (recomended_peer) REFERENCES peers(nickname)
+    recomended_peer VARCHAR NOT NULL
 );
 CREATE TABLE xp (
     id SERIAL PRIMARY KEY,
-    check_num BIGINT NOT NULL,
-    xp_amount INT NOT NULL,
-    CONSTRAINT fk_xp_check_num FOREIGN KEY (check_num) REFERENCES checks(id)
+    check_num INT NOT NULL,
+    xp_amount INT NOT NULL
 );
 CREATE TABLE time_tracking (
     id SERIAL PRIMARY KEY,
     peer VARCHAR,
     date_state DATE NOT NULL,
     time_state TIME NOT NULL,
-    peer_state INT CHECK (peer_state IN (1, 2)),
-    CONSTRAINT fk_time_tracking_peer FOREIGN KEY (peer) REFERENCES peers(nickname)
+    peer_state INT CHECK (peer_state IN (1, 2))
 );
+ALTER TABLE checks
+ADD CONSTRAINT fk_checks_peer FOREIGN KEY (peer) REFERENCES peers(nickname);
+ALTER TABLE checks
+ADD CONSTRAINT fk_checks_task FOREIGN KEY (task) REFERENCES tasks(title);
+ALTER TABLE time_tracking
+ADD CONSTRAINT fk_time_tracking_peer FOREIGN KEY (peer) REFERENCES peers(nickname);
+ALTER TABLE p2p
+ADD CONSTRAINT fk_p2p_check_num FOREIGN KEY (check_num) REFERENCES checks(id);
+ALTER TABLE p2p
+ADD CONSTRAINT uk_p2p_check UNIQUE (check_num, checking_peer, check_state);
+-- WHERE check_state IN ('Start');
+ALTER TABLE friend
+ADD CONSTRAINT fk_friend_peer1 FOREIGN KEY (peer1) REFERENCES peers(nickname);
+ALTER TABLE friend
+ADD CONSTRAINT fk_friend_peer2 FOREIGN KEY (peer2) REFERENCES peers(nickname);
+ALTER TABLE friend
+ADD CONSTRAINT uk_peer1_peer2 UNIQUE (peer1, peer2);
+-- ALTER TABLE friend
+-- ADD CONSTRAINT chk_peer1_peer2 CHECK (peer1 < peer2);
+ALTER TABLE recomendations
+ADD CONSTRAINT fk_recomendations_peer FOREIGN KEY (peer) REFERENCES peers(nickname);
+ALTER TABLE recomendations
+ADD CONSTRAINT fk_recomendations_recomended_peer FOREIGN KEY (recomended_peer) REFERENCES peers(nickname);
+ALTER TABLE xp
+ADD CONSTRAINT fk_xp_check_num FOREIGN KEY (check_num) REFERENCES checks(id);
 -- DROP PROCEDURE import_csv_data;
 -- Create procedure to import in csv file 
 CREATE OR REPLACE PROCEDURE import_csv_data(
@@ -116,13 +122,33 @@ CALL import_csv_data(
     ','
 );
 CALL import_csv_data(
+    'checks',
+    '/home/janiecee/Documents/github/SQL2_Info21_v1.0-1/src/Checks.csv',
+    ','
+);
+CALL import_csv_data(
     'p2p',
     '/home/janiecee/Documents/github/SQL2_Info21_v1.0-1/src/P2P.csv',
     ','
 );
 CALL import_csv_data(
-    'checks',
-    '/home/janiecee/Documents/github/SQL2_Info21_v1.0-1/src/Checks.csv',
+    'p2p',
+    '/home/janiecee/Documents/github/SQL2_Info21_v1.0-1/src/P2P.csv',
+    ','
+);
+CALL import_csv_data(
+    'verter',
+    '/home/janiecee/Documents/github/SQL2_Info21_v1.0-1/src/Verter.csv',
+    ','
+);
+CALL import_csv_data(
+    'transfered_points',
+    '/home/janiecee/Documents/github/SQL2_Info21_v1.0-1/src/Transfered_points.csv',
+    ','
+);
+CALL import_csv_data(
+    'friend',
+    '/home/janiecee/Documents/github/SQL2_Info21_v1.0-1/src/Friends.csv',
     ','
 );
 DROP OWNED BY janiecee;
