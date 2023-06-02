95f0378a8e635e4d0ff6dc5a4f26e1f4fd60a804
Janiece Eleonor <janiecee@un-j4.kzn.21-school.ru>
2023-06-02 14:01:18 +0300

    develop-janiecee PART 3 TASK 4 OK

--- src/task_3/part3.sql
+++ src/task_3/part3.sql
@@ -1,4 +1,4 @@
--- 1) Write a function that returns the TransferredPoints table in a more human-readable form
+-- 1) Write a function that returns the transfered_points table in a more human-readable form
 CREATE OR REPLACE FUNCTION tp_human_view () RETURNS TABLE (
         Peer1 VARCHAR,
         Peer2 VARCHAR,
@@ -54,13 +54,32 @@ FROM peer_not_left('2023-06-01');
 -- VALUES (7, 'shoredim', '2023-06-01', '09:00:00', 1);
 -- INSERT INTO time_tracking
 -- VALUES (8, 'shoredim', '2023-06-02', '00:01:00', 2);
--- 4) Calculate the change in the number of peer points of each peer using the TransferredPoints table
-CREATE OR REPLACE PROCEDURE prp_change (IN cursor REFCURSOR = 'result_4') AS $$ BEGIN OPEN result_4 FOR
-SELECT nickname,
-    SUM(COALESCE(transfered_points.points_amount, 0))
-FROM peers
-    LEFT JOIN transfered_points ON transfered_points.checking_peer = peers.nickname
-GROUP BY peers.nickname
-ORDER BY nickname
+-- 4) Calculate the change in the number of peer points of each peer using the transfered_points table
+CREATE OR REPLACE PROCEDURE prp_change (IN cursor REFCURSOR = 'result_p3_t4') AS $$ BEGIN OPEN cursor FOR WITH Peer1Trans AS (
+        SELECT nickname,
+            SUM(COALESCE(transfered_points.points_amount, 0)) AS total
+        FROM peers
+            LEFT JOIN transfered_points ON transfered_points.checking_peer = peers.nickname
+        GROUP BY peers.nickname
+        ORDER BY nickname
+    ),
+    Peer2Trans AS (
+        SELECT nickname,
+            SUM(COALESCE(transfered_points.points_amount, 0)) AS total
+        FROM peers
+            LEFT JOIN transfered_points ON transfered_points.checked_peer = peers.nickname
+        GROUP BY peers.nickname
+        ORDER BY nickname
+    )
+SELECT Peer2Trans.nickname AS Peer,
+    Peer2Trans.total - Peer1Trans.total AS ChangePoints
+FROM Peer2Trans
+    JOIN Peer1Trans ON Peer1Trans.nickname = Peer2Trans.nickname
+ORDER BY ChangePoints DESC;
 END;
 $$ LANGUAGE plpgsql;
+-- DROP PROCEDURE prp_change;
+-- Check procedure
+CALL prp_change();
+FETCH ALL
+FROM "result_p3_t4";
