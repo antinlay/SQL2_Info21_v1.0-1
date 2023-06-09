e867c47fd8f2265815da4fb7ccc50c73fccdbb0b
Janiece Eleonor <janiecee@un-j4.kzn.21-school.ru>
2023-06-10 02:35:45 +0300

    develop_janiecee PART 4 START DESTROY TABLES WITH PREFIX

--- src/part4.sql
+++ src/part4.sql
@@ -1,2 +1,26 @@
 -- 1) Create a stored procedure that, without destroying the database, destroys all those tables in the current database whose names begin with the phrase 'TableName'.
-CREATE OR REPLACE PROCEDURE
+CREATE OR REPLACE FUNCTION destroy_tables(_prefix text) RETURNS void AS $func$
+DECLARE loop_record text;
+BEGIN FOR loop_record IN
+SELECT quote_ident(table_schema) || '.' || quote_ident(table_name)
+FROM information_schema.tables
+WHERE table_type = 'BASE TABLE'
+    AND table_name LIKE (_prefix || '%') LOOP EXECUTE 'DROP TABLE ' || loop_record || ' CASCADE';
+END LOOP;
+END $func$ LANGUAGE plpgsql;
+SELECT destroy_tables('p');
+-- GPT FUNCTION !!!!!!
+CREATE OR REPLACE PROCEDURE desctroy_tables(IN name_pattern VARCHAR) LANGUAGE plpgsql AS $$
+DECLARE loop_record record;
+BEGIN FOR loop_record IN
+SELECT *
+FROM information_schema.tables
+SELECT table_schema,
+    table_name
+FROM information_schema.tables
+WHERE table_schema LIKE 'public' || '%'
+    AND table_type = 'BASE TABLE' LOOP EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(loop_record.table_schema) || '.' || quote_ident(loop_record.table_name) || ' CASCADE';
+RAISE NOTIICE 'Table deleted: %',
+quote_ident(loop_record.table_schema) || '.' || quote_ident(loop_record.table_name);
+END LOOP;
+END;
